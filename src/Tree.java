0dc33c4a91c4d8d8294083c90e439172d5b6a0c9
Dharma <dharma.ong@mail.utoronto.ca>
2023-09-18 16:42:45 -0400

    added stuff

--- src/Tree.java
+++ src/Tree.java
@@ -1,9 +1,14 @@
-import java.util.ArrayList
+import java.util.ArrayList;
 
-public class Tree {
+public class Tree extends MultisetADT {
     private int root;
     private ArrayList<Tree> subtrees;
 
+    public Tree(int root, ArrayList<Tree> subtrees) {
+        this.root = root;
 
+        if subtrees
+        this.subtrees = subtrees;
+    }
 
 }
